8e16045afbb10ef88fbfb0faa0212dc618bcca70
HGF <hawkeyegamefilm@gmail.com>
2023-05-19 10:12:52 -0500

    Adding May data update

--- transfer_portal_chronology.py
+++ transfer_portal_chronology.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-df = pd.read_csv('data/transfer_portal_2023_2_22_2023.csv')
+df = pd.read_csv('data/transfer_portal_2023_05_03.csv')
 df['date'] = pd.DatetimeIndex(df.transferDate).normalize()
 s = df.groupby('date').date.agg('count').to_frame('cnt').reset_index().sort_values('date', ascending=False)
 
@@ -11,7 +11,10 @@ print(s2.to_string())
 fig,ax = plt.subplots()
 ax.plot_date(s2.date, s2.cnt, linestyle='--')
 
-ax.annotate('Dec 5', (mdates.date2num(s2.loc[80].date), s2.loc[80].cnt), xytext=(100, 25),
+ax.annotate('Dec 5, 2022', (mdates.date2num(s2.loc[79].date), s2.loc[79].cnt), xytext=(40, 1),
+            textcoords='offset points', arrowprops=dict(arrowstyle='-|>'))
+
+ax.annotate('April 15, 2023', (mdates.date2num(s2.loc[174].date), s2.loc[174].cnt), xytext=(40, 1),
             textcoords='offset points', arrowprops=dict(arrowstyle='-|>'))
 
 fig.autofmt_xdate()
